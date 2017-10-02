594b8d2855df4395e5b297f1b45ae653b16306f6
anasantos11 <anasantos1103@gmail.com>
2017-10-02 18:01:13 -0300

    Implementado método que retorna objeto do tipo "GSON" com todos os diagnósticos cadastrados até o momento

--- kanLeitos-workspace/kanleitos/src/main/java/br/com/kanleitos/controllers/DiagnosticoController.java
+++ kanLeitos-workspace/kanleitos/src/main/java/br/com/kanleitos/controllers/DiagnosticoController.java
@@ -5,9 +5,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import br.com.kanleitos.models.Diagnostico;
 import br.com.kanleitos.repository.DiagnosticoRepository;
-import br.com.kanleitos.service.models.DiagnosticoResposta;
 
 @Controller
 public class DiagnosticoController{
@@ -17,21 +19,10 @@ public class DiagnosticoController{
 
 	@RequestMapping(value = "Diagnosticos", method = org.springframework.web.bind.annotation.RequestMethod.GET)
 	public @ResponseBody String listarDiagnosticos() throws JSONException {
-		boolean erroFlag = true;
-
 		Iterable<Diagnostico> diagnosticos = repository.findAll();
-		
-		erroFlag = false;
-		DiagnosticoResposta responseObject = new DiagnosticoResposta(erroFlag);
-		diagnosticos.forEach(x ->{
-			responseObject.setIdDiagnostico(Integer.toString(x.getIdDiagnostico()));
-			responseObject.setCID(x.getCID());
-			responseObject.setDescricaoDiagnostico(x.getDescricaoDiagnostico());
-			responseObject.setTempoPermanencia(Integer.toString(x.getTempoPermanencia()));
-		});
-
-		String response = responseObject.toJson().toString();
-		return response;
+		Gson gson = new GsonBuilder().create();
+		String d = gson.toJson(diagnosticos);
+		return d;
 	}
 
 }
