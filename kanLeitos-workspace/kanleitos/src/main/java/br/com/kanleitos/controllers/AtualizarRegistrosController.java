ed4714db7d0f19f3a5f58d377d1f0f3279721618
anasantos11 <anasantos1103@gmail.com>
2017-10-26 01:37:22 -0200

    Implementado método para atualizar classificacao dos registros de internação

--- kanLeitos-workspace/kanleitos/src/main/java/br/com/kanleitos/controllers/AtualizarRegistrosController.java
+++ kanLeitos-workspace/kanleitos/src/main/java/br/com/kanleitos/controllers/AtualizarRegistrosController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import br.com.kanleitos.models.RegistroInternacao;
 import br.com.kanleitos.repository.RegistroInternacaoRepository;
+import br.com.kanleitos.util.Classificacao;
 import br.com.kanleitos.util.StatusRegistro;
 
 @Controller
@@ -22,18 +23,32 @@ public class AtualizarRegistrosController {
 
 	@RequestMapping(value = "AtualizarInternacoes", method = org.springframework.web.bind.annotation.RequestMethod.GET)
 	public @ResponseBody int atualizarInternacoes() throws JSONException {
-		List<RegistroInternacao> registros = registroRepository.findByStatusRegistro(StatusRegistro.EM_ANDAMENTO);		
+		List<RegistroInternacao> registros = registroRepository.findByStatusRegistro(StatusRegistro.EM_ANDAMENTO);
 		registros.forEach(x -> {
 			x.setDiasInternacao(calcularDiasInternados(x.getDataInternacao()));
+			x.setClassificacao(calcularClassificacao(x));
 		});
 		registroRepository.save(registros);
 		return 0;
 	}
-	
+
 	public static int calcularDiasInternados(String data) {
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 		LocalDateTime dataAdmissaoTime = LocalDateTime.parse(data, formatter);
 		Duration duracao = Duration.between(dataAdmissaoTime, LocalDateTime.now());
 		return (int) duracao.toDays();
+	}
+
+	public static Classificacao calcularClassificacao(RegistroInternacao registro) {
+		int tempoPermanenciaDiagnostico = registro.getPedidoInternacao().getDiagnostico().getTempoPermanencia();
+		float percentual = (float) (registro.getDiasInternacao() * 100) / tempoPermanenciaDiagnostico;
+
+		if (percentual <= 50) {
+			return Classificacao.VERDE;
+		} else if (percentual <= 100) {
+			return Classificacao.AMARELO;
+		} else {
+			return Classificacao.VERMELHO;
+		}
 	}
 }
