3f6783adadb523c9a38d3095d6e556c75edbfa9d
anasantos11 <anasantos1103@gmail.com>
2017-12-12 23:08:45 -0200

    Incluso firebase para processo de login / Implementado autenticacao do usuario

--- webApp/app.js
+++ webApp/app.js
@@ -1,14 +1,57 @@
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyB5CnszQphntRXBUpc8Kisk1_WqOJesaKo",
+	authDomain: "kanleitos.firebaseapp.com",
+	databaseURL: "https://kanleitos.firebaseio.com",
+	projectId: "kanleitos",
+	storageBucket: "kanleitos.appspot.com",
+	messagingSenderId: "573530601324"
+};
+firebase.initializeApp(config);
+
+var isLogado = false;
+
 var app = angular.module("kanleitos", [
+	'firebase',
 	'ui.router',
 	"ngDialog",
 	"720kb.tooltips",
 	"ui.utils.masks"]);
 
+app.controller('KanController', ['$rootScope', '$scope', '$state', '$firebaseAuth', function ($rootScope, $scope, $state, $firebaseAuth) {
+	$scope.auth = $firebaseAuth();
+	$scope.auth.$onAuthStateChanged(function (user) {
+		if (user != null) {
+			$scope.nomeUsuario = user.displayName;
+			$scope.emailUsuario = user.email;
+			isLogado = true;
+			location.href = "#!/home";
+		}
+	});
+
+	$scope.sairAplicacao = function () {
+		firebase.auth().signOut()
+			.then(function () {
+				$('#sairAppModal').modal('hide');
+				location.href = "#!/login";
+				isLogado = false;
+			}).catch(function (error) {
+				swal(
+					"Algum erro ocorreu, tente novamente!",
+					"",
+					error
+				)
+			});
+
+	};
+}]);
+
+
 app.config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
 	$urlRouterProvider.otherwise("/login");
 
-	$stateProvider.state("login",{
-		url : "/login",
+	$stateProvider.state("login", {
+		url: "/login",
 		templateUrl: "login.html"
 	});
 
@@ -43,17 +86,20 @@ app.config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
 	});
 })
 
-.run(['$rootScope', '$location',
-    function ($rootScope, $location) {
+app.run(['$rootScope', '$location',
+	function ($rootScope, $location) {
 
-        $rootScope.$on('$locationChangeStart', function (event, next, current) {
-            
-            if ($location.path() == '/login') {
+		$rootScope.$on('$locationChangeStart', function (event, next, current) {
+			var rota = $location.path();
+			if (rota == '/login' || rota == '/' || rota == '') {
 				$rootScope.IsLogin = true;
-				
-            }else{
+			} else if (rota != '/' && rota != '') {
 				$rootScope.IsLogin = false;
+				if (!isLogado) {
+					event.preventDefault();
+					location.href = "#!/login";
+				}
 			}
-        });
-}]);
+		});
+	}]);
 
--- webApp/resources/js/controllers/usuario/loginController.js
+++ webApp/resources/js/controllers/usuario/loginController.js
@@ -1,4 +1,5 @@
-app.controller('loginController', ["$scope", "$http","usuarioFactory",function ($scope, $http, usuarioFactory) {    
+app.controller('loginController', ["$scope", "$http", "usuarioFactory", function ($scope, $http, usuarioFactory) {
+
     $scope.NovoLogin = function () {
         $scope.login = {
             login: "",
@@ -7,7 +8,7 @@ app.controller('loginController', ["$scope", "$http","usuarioFactory",function (
     }
     $scope.NovoLogin();
 
-    $scope.validarLogin = function () {
+    $scope.validarCamposLogin = function () {
         if ($scope.login.login == "") {
             swal(
                 'Erro!',
@@ -28,69 +29,34 @@ app.controller('loginController', ["$scope", "$http","usuarioFactory",function (
         return true;
     }
 
-    $scope.realizarLogin = function (login) {
-        if ($scope.validarLogin()) {
-            $http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded; charset=utf-8";
-           // $scope.login.senha = codificar($scope.login.senha, "12345")
-           // $scope.login.senha = decodificarParaSHA256($scope.login.senha, "12345")
-            usuarioFactory.login($scope.login)
-                .then(function (response) {
-                    if (response.data.usuarioValidado) {
-                        location.href = "#!/home";
-                    } else {
-                        swal(
-                            'Erro!',
-                            'Usuário inválido',
-                            'error'
-                        )
-                    }
 
-                }, function (response) {
-                    if (response.data != undefined) {
-                        swal(
-                            'Erro!',
-                            response.data.message,
-                            'error'
-                        )
-                    } else {
-                        swal(
-                            'Erro!',
-                            'Ocorreu algum erro no servidor',
-                            'error'
-                        )
-                    }
+    $scope.verificarErro = function (codigoErro) {
+        if (codigoErro == 'auth/invalid-email') {
+            return "E-mail inválido."
+        }
+        if(codigoErro == 'auth/user-not-found'){
+            return "Usuário não encontrado.";
+        }
+        if(codigoErro == 'auth/wrong-password'){
+            return "Senha incorreta";
+        }
+
+        return "Verifique os dados e tente novamente.";
+
+    };
+
+    $scope.realizarLogin = function () {
+        if ($scope.validarCamposLogin()) {
+            firebase.auth().signInWithEmailAndPassword($scope.login.login, $scope.login.senha)
+                .catch(function (error) {
+                    swal(
+                        $scope.verificarErro(error.code),
+                        "",
+                        'error'
+                    )
+
                 });
         }
-    }
-    //CRIPTOGRAFIA BASEADA EM SHA256
-    
-    //DADO = valor que deseja codificar
-    //CHAVE = string que recebe a chave de codificação, pode ser qualquer coisa
-            //sendo que a mesma deve ser guardada para decodificação
-            //exemplo de chave: 123456
-    const codificar = (dado, chave)=>{
-        return CryptoJS.AES.encrypt(dado, chave).toString()
-    }
-    
-    //HASHCODIFICADA = dado que já esta encryptado que ira ser decodificado caso necessário
-    //CHAVEDEDECODIFICACAO = valor que deve ser usado como chave para decodificação da hash
-                            // pode ser qualuqer valor em string
-                            //exemplo de chave 123456
-    const decodificar = (hashCodificada, chaveDeDecodificacao)=>{
-        var bytes  = CryptoJS.AES.decrypt(hashCodificada, chaveDeDecodificacao)
-        return bytes.toString(CryptoJS.enc.Utf8)
-    }
+    };
 
-    //HASHCODIFICADA = dado que já esta encryptado que ira ser decodificado para SHA256 caso necessário
-    //CHAVEDEDECODIFICACAO = valor que deve ser usado como chave para decodificação da hash
-                            // pode ser qualuqer valor em string
-                            //exemplo de chave 123456
-    const decodificarParaSHA256 = (hashCodificada, chaveDeDecodificacao)=>{
-        var bytes  = CryptoJS.AES.decrypt(hashCodificada, chaveDeDecodificacao)
-        return bytes.toString()
-    }
-
-    //EXEMPLO
-    // var dadoCodificado = codificar("Luiz Henrique Silva Jesus", "12345");
-    // console.log(decodificarParaSHA256(dadoCodificado, "12345"))
 }]);
