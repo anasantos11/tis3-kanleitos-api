8e46121471186d2b4a8a1ca88578962a076c1bca
anasantos11 <anasantos1103@gmail.com>
2017-12-18 23:41:37 -0200

    Ajustado metodo que calcular tempo decorrido da espera da internacao para mostrar dias, horas e minutos

--- webApp/resources/js/controllers/internacao/ModalPesquisaPedidoInternacaoController.js
+++ webApp/resources/js/controllers/internacao/ModalPesquisaPedidoInternacaoController.js
@@ -1,18 +1,31 @@
 app.controller('ModalPesquisaPedidoInternacaoController', ["$scope", "pedidoInternacaoFactory",
     function ($scope, pedidoInternacaoFactory) {
-
-
         $scope.calcularHorasAguardando = function (dataAtual, dataAdmissao) {
-            var diffMs = (dataAtual - dataAdmissao);
-            var diffHrs = Math.floor((diffMs % 86400000) / 3600000);
-            var diffMins = Math.round(((diffMs % 86400000) % 3600000) / 60000);
-            return diffHrs + 'h ' + diffMins + 'min';
+            var one_day = 1000 * 60 * 60 * 24;
+
+            // Convert both dates to milliseconds
+            var date1_ms = dataAdmissao.getTime();
+            var date2_ms = dataAtual.getTime();
+
+            // Calculate the difference in milliseconds
+            var difference_ms = date2_ms - date1_ms;
+            //take out milliseconds
+            difference_ms = difference_ms / 1000;
+            var seconds = Math.floor(difference_ms % 60);
+            difference_ms = difference_ms / 60;
+            var minutes = Math.floor(difference_ms % 60);
+            difference_ms = difference_ms / 60;
+            var hours = Math.floor(difference_ms % 24);
+            var days = Math.floor(difference_ms / 24);
+
+            return days + ' dia(s) ' + hours + 'h ' + minutes + 'min';
+
 
         };
 
         $scope.atualizarHorasAguardando = function () {
             var dataAtual = new Date();
-            for (var i = 0;i < $scope.listaPedidos.length; i++) {
+            for (var i = 0; i < $scope.listaPedidos.length; i++) {
                 $scope.listaPedidos[i].horasAguardando = $scope.calcularHorasAguardando(dataAtual, new Date($scope.listaPedidos[i].dataAdmissao))
             };
         };
