e14e046fae5dc3aefa3ef959e935c8e7854277f6
Ana <anasantos1103@gmail.com>
2018-02-10 19:43:55 -0200

    Criado tarefas no gulp para copiar arquivos do src / Unificado codigo css do kanleitos

--- webApp/Gulpfile.js
+++ webApp/Gulpfile.js
@@ -1,6 +1,59 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var mainBowerFiles = require('gulp-main-bower-files');
+var replace = require('gulp-replace');
+
+
+//Arquivos JS que serão concatenados e salvo em dist
+var jsAppConcat = [
+    "src/app.js",
+    "src/resources/factorys/*.js",
+    "src/resources/services/*.js",
+    "src/resources/controllers/**/*/*.js",
+];
+
+//Arquivos HTML que serão apenas copiados para dist
+var htmlAppCopy = [
+    "src/templates/**/*"
+];
+
+//Arquivos kan libs que serão apenas copiados para dist
+var libsAppCopy = [
+    "src/resources/libs/**/*"
+];
+
+///Tarefas para copiar templates para dist
+gulp.task('copy-templates', function () {
+    gulp.src(htmlAppCopy)
+        .pipe(gulp.dest('dist/templates'));
+});
+
+///Tarefas para copiar index.html para dist
+gulp.task('copy-index-html', function () {
+    gulp.src("src/index.html")
+        .pipe(replace('../dist/', ''))
+        .pipe(gulp.dest('dist/'));
+});
+
+///Tarefas para copiar pasta img para dist
+gulp.task('copy-img', function () {
+    gulp.src("src/img/**/*")
+        .pipe(gulp.dest('dist/img'));
+});
+
+///Tarefa de concatenação dos arquivos JS - Verificar array jsAppConcat
+gulp.task('concat-kan-js', function () {
+    gulp.src(jsAppConcat)
+        .pipe(concat('app.js'))
+        .pipe(gulp.dest('dist/'));
+});
+
+///Tarefa para copiar libs kan para dist
+gulp.task('copy-kan-libs', function () {
+    gulp.src(libsAppCopy)
+        .pipe(gulp.dest('dist/libs'));
+
+});
 
 //Tarefa para copiar as fonts do font-awesome para pasta dist/libs/fonts 
 gulp.task('libs-fonts', function () {
@@ -20,10 +73,9 @@ gulp.task('libs-css', function () {
                 }
             }
         }))
-        .pipe(concat('myApp-libs.css'))
+        .pipe(concat('public-libs.css'))
         .pipe(gulp.dest('dist/libs/css'))
 });
-
 
 //Tarefa para copiar os arquivos .js para pasta dist/libs/js  em um unico arquivo e minificar o codigo
 gulp.task('libs-js', function () {
@@ -44,12 +96,21 @@ gulp.task('libs-js', function () {
                 }
             }
         }))
-        .pipe(concat('myApp-libs.js'))
+        .pipe(concat('public-libs.js'))
         .pipe(gulp.dest('dist/libs/js'))
 });
 
 // Tarefa padrão quando executado o comando GULP
-gulp.task('default', ["libs-css", "libs-js", "libs-fonts"]);
+gulp.task('default', [
+    "libs-css",
+    "libs-js",
+    "libs-fonts",
+    "copy-templates",
+    "copy-index-html",
+    "copy-img",
+    "concat-kan-js",
+    "copy-kan-libs"]
+);
 
 
 
